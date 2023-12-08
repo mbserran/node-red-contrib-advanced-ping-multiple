ac6b8f136c664f324a4a4a04e372751738b7112d
Javier Serrano <mbserran@gmail.com>
2023-12-09 00:31:55 +0100

    Add files via upload

--- README.md
+++ README.md
@@ -9,20 +9,20 @@ This is a modification of the [node-red-node-ping](https://github.com/node-red/n
 
 Run the following command in the root directory of your Node-RED install
 
-    npm install node-red-contrib-advanced-ping
+    npm install node-red-contrib-advanced-ping-multiple
 
 ## Usage
 
 * Pings a machine and returns the trip time in ms. Ping time is returned in `msg.payload`. Returns boolean **false** if no response received within 5 seconds, or if the host is unresolveable.
 * Will perform ping on **any** input.
-* You may override the host set in the configuration by passing in a value in `msg.host`.
+* You may override the host set in the configuration by passing in a value in `msg.host`. `msg.host`can be an array of hosts. In that case `msg.payload`will be an array with the delays or -1000 when unreachable.
 * For legacy reasons, the node will output the host as `msg.topic` (the original [node-red-node-ping](https://github.com/node-red/node-red-nodes/tree/master/io/ping) does it this way).
 * Any incomming data will be passed on to the output.
   * Incoming `msg.payload` data will be outputted as `msg._payload` (as the new `msg.payload` will contain the result of the ping).
   * Incoming `msg.topic` data will be outputted as `msg._topic` (as the new `msg.topic` will contain the the host).
 
 ## Todo
-Ability to set ping options in node configuration/by incomming message. Right now, these settings are hardcoded
+Ability to set ping options in node configuration/by incomming message. Right now, these settings are hardcoded. You can override the timeout by passing a msg.timeout value in seconds.
 
 * Linux: -n -w 5 -c 1
 * Windows: -n 1 -w 5000
---
+++ ping-many/ping-many.js
@@ -0,0 +1,90 @@
+/**
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **/
+
+module.exports = function(RED) {
+    "use strict";
+    var spawn = require('child_process').spawn;
+    var plat = require('os').platform();
+
+    function PingNode(n) {
+        RED.nodes.createNode(this,n);
+        this.host = n.host;
+        var node = this;
+		
+		this.on("input", function (msg) {
+			var host = msg.host || node.host;
+			
+			function MapPing(value, index, array) {
+				var ex;
+				if (plat == "linux") { ex = spawn('ping', ['-n', '-w', tout,toString(), '-c', '1', value]); }
+				else if (plat.match(/^win/)) { ex = spawn('ping', ['-n', '1', '-w', (1000*tout).toString(), value]); }
+				else if (plat == "darwin") { ex = spawn('ping', ['-n', '-t', tout.toString(), '-c', '1', value]); }
+				else { node.error("Sorry - your platform - "+plat+" - is not recognised."); }
+				var res = false;
+				var line = "";
+				//var regex = /from.*time.(.*)ms/;
+				var regex = /=.*[<|=]([0-9]*).*TTL|ttl..*=([0-9\.]*)/;
+				ex.stdout.on('data', function (data) {
+					line += data.toString();
+				});
+				ex.stderr.on('data', function (data) {
+					//console.log('[ping] stderr: ' + data);
+				});
+				ex.on('close', function (code) {
+					var m = regex.exec(line)||"";
+					if (m !== '') {
+						if (m[1]) { res = Number(m[1]); }
+						if (m[2]) { res = Number(m[2]); }
+					}
+					if (code === 0) {
+						return res;
+					} else {
+						return -1000;  // Value to indicate no response to ping
+					}
+				});
+			}
+
+			if(msg.hasOwnProperty('payload')) {
+				msg._payload = msg.payload;
+			}
+			if(msg.hasOwnProperty('topic')) {
+				msg._topic = msg.topic;
+			}
+			if(msg.hasOwnProperty('timeout')) {
+				var tout = (isNaN(msg.timeout)?5:parseInt(msg.timeout,10));
+			} else {
+				var tout = 5;
+			}
+			msg.payload = {};
+			msg.topic = host;
+
+			if (!host) {
+				node.warn('No host is specificed. Either specify in node configuration or by passing in msg.host');
+			}
+			
+			if (Array.isArray(host)) {
+				const delay = host.map(MapPing);
+			} else {
+				let delay = MapPing(host,null, null);
+			}				
+
+			msg.payload = delay;
+			
+			node.send(msg);
+
+		});
+
+    }
+    RED.nodes.registerType("ping many",PingNode);
+}
